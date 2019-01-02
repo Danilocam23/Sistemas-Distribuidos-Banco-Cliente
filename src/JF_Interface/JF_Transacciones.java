3e59e8aab8daa07c597f613bd93c048289bafd07
Danilo Camacho <xcojcama@intcomex.com>
2019-01-02 15:59:03 -0500

    Formulario de Transaciones

--- src/JF_Interface/JF_Transacciones.java
+++ src/JF_Interface/JF_Transacciones.java
@@ -5,6 +5,8 @@
  */
 package JF_Interface;
 
+import banco_cliente.Conectar_Sockets;
+
 /**
  *
  * @author Danilo
@@ -27,21 +29,121 @@ public class JF_Transacciones extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        bG_transacciones = new javax.swing.ButtonGroup();
+        jR_consignacion = new javax.swing.JRadioButton();
+        jR_tran_retiro = new javax.swing.JRadioButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jT_tran_numero = new javax.swing.JTextField();
+        jT_tran_dinero = new javax.swing.JTextField();
+        jB_guardar = new javax.swing.JButton();
+        jB_tran_cancelar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(358, 359));
+
+        bG_transacciones.add(jR_consignacion);
+        jR_consignacion.setText("Consignacion");
+        jR_consignacion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jR_consignacionActionPerformed(evt);
+            }
+        });
+
+        jR_tran_retiro.setText("Retiro");
+
+        jLabel1.setText("Número de la cuenta");
+
+        jLabel2.setText("Cantidad del dinero");
+
+        jB_guardar.setText("Guardar");
+        jB_guardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jB_guardarActionPerformed(evt);
+            }
+        });
+
+        jB_tran_cancelar.setText("Cancelar");
+        jB_tran_cancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jB_tran_cancelarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(21, 21, 21)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel1))
+                                .addGap(39, 39, 39)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jT_tran_numero)
+                                    .addComponent(jT_tran_dinero, javax.swing.GroupLayout.DEFAULT_SIZE, 225, Short.MAX_VALUE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jR_consignacion)
+                                .addGap(18, 18, 18)
+                                .addComponent(jR_tran_retiro)
+                                .addGap(0, 0, Short.MAX_VALUE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(90, 90, 90)
+                        .addComponent(jB_guardar)
+                        .addGap(18, 18, 18)
+                        .addComponent(jB_tran_cancelar)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(91, 91, 91)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jR_consignacion)
+                    .addComponent(jR_tran_retiro))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jT_tran_numero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jT_tran_dinero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jB_guardar)
+                    .addComponent(jB_tran_cancelar))
+                .addGap(40, 40, 40))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jR_consignacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jR_consignacionActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jR_consignacionActionPerformed
+
+    private void jB_tran_cancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jB_tran_cancelarActionPerformed
+        
+        jT_tran_numero.setText("");
+        jT_tran_dinero.setText("");
+    }//GEN-LAST:event_jB_tran_cancelarActionPerformed
+
+    private void jB_guardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jB_guardarActionPerformed
+       String value
+                = jT_tran_numero.getText() + ","
+                + jT_tran_dinero.getText();
+
+        Conectar_Sockets cs = new Conectar_Sockets();
+
+        cs.Datos(value);
+    }//GEN-LAST:event_jB_guardarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +181,14 @@ public class JF_Transacciones extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup bG_transacciones;
+    private javax.swing.JButton jB_guardar;
+    private javax.swing.JButton jB_tran_cancelar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JRadioButton jR_consignacion;
+    private javax.swing.JRadioButton jR_tran_retiro;
+    private javax.swing.JTextField jT_tran_dinero;
+    private javax.swing.JTextField jT_tran_numero;
     // End of variables declaration//GEN-END:variables
 }
